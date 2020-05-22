c4e2b8fa027667a224c98139342ac71624f688c8
shrutee2001 <65718930+shrutee2001@users.noreply.github.com>
2020-05-22 20:03:35 +0530

    Create Program5.py

---
+++ Day1/Program5.py
@@ -0,0 +1,15 @@
+p1=int(input("Enter runs scored by First player in 60 balls="))
+p2=int(input("Enter runs scored by Second player in 60 balls="))
+p3=int(input("Enter runs scored by Third player in 60 balls="))
+st1=p1*100/60
+st2=p2*100/60
+st3=p3*100/60
+print("Strike rate of First player is=",st1)
+print("Strike rate of Second player is=",st2)
+print("Strike rate of Third player is=",st3)
+print("Runs scored by First player if he playes 60 balls more=",p1*2)
+print("Runs scored by Second player if he playes 60 balls more=",p2*2)
+print("Runs scored by Third player if he playes 60 balls more=",p3*2)
+print("Six hit by First player=",p1//6)
+print("Six hit by Second player=",p2//6)
+print("Six hit by Third player=",p3//6)
