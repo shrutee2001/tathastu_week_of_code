d0947c3729f231f9e2684b149efa4bc74c7764eb
shrutee2001 <65718930+shrutee2001@users.noreply.github.com>
2020-05-22 19:59:49 +0530

    Create Program3.py

---
+++ Day1/Program3.py
@@ -0,0 +1,7 @@
+a=int(input("Enter First number="))
+b=int(input("Enter Second number="))
+print("the values of a and b before swapping:",(a,b))
+a=a+b
+b=a-b
+a=a-b
+print("the values of a and b after swapping:",(a,b))
