4d8641218c041008e74c7c690ba420a0696b56f1
shrutee2001 <65718930+shrutee2001@users.noreply.github.com>
2020-05-22 20:03:03 +0530

    Create Program4.py

---
+++ Day1/Program4.py
@@ -0,0 +1,7 @@
+cp=int(input("Enter cost price of a product="))
+sp=int(input("Enter selling price of a product="))
+p=sp-cp
+newsellingprice=1.05*p+cp
+print("Profit is=",p)
+print("New Selling price is=",newsellingprice)
+
