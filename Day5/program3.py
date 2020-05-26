6d2bbfb06d342ad8d67e284acefaff5c978ddf60
shrutee2001 <65718930+shrutee2001@users.noreply.github.com>
2020-05-26 21:04:48 +0530

    Update program3.py

--- Day5/program3.py
+++ Day5/program3.py
@@ -1,4 +1,13 @@
-def replace_0_with_5(number):
-    return int(str(number).replace('0','5'))
-number = int(input("Enter the number you want to check: "))
-print("The number after replacing all 0 with 5 will be", replace_0_with_5(number))
+def House(List):
+    if len(List) == 2:
+        return max(List)
+    if len(List) == 1:
+        return List[0]
+    if len(List) == 3:
+        return max(List[1], List[0] + House(List[2:]))
+    return max(List[1] + House(List[3:]), List[0] + House(List[2:]))
+size = int(input("Enter the number of Houses in a line: "))
+List = []
+for i in range(size):
+    List.append(int(input("Enter the value in the House number " + str(i + 1) + ": ")))
+print("The maximum stolen value from Houses is", House(List))
